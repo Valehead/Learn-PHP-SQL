3f3d4cf0233166d24a208d168ae24e18c0d2a837
Valehead <iquicksnipe@gmail.com>
2022-02-02 01:08:34 -0500

    testing sql

---
+++ connect.php
@@ -0,0 +1,42 @@
+<?php
+
+$new_user = [
+    "name" => "",
+    "phone" => "",
+    "email" => "",
+    "birthday" => ""
+];
+
+
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+    $mysqli = mysqli_connect('localhost', 'valehead', 'the4kingdb$', 'ripnship') or die("Connection Failed: " . mysqli_connect_error());
+
+    if(isset($_POST['name']) && isset($_POST['phone']) && isset($_POST['email']) && isset($_POST['birthday'])){
+        foreach($new_user as $key => $value){
+            $new_user[$key] = $_POST[$key];
+        };
+        // $name = $_POST['name'];
+        // $phone = $_POST['phone'];
+        // $email = $_POST['email'];
+        // $birthday = $_POST['birthday'];
+
+    $sql_query = "INSERT INTO `customers` (`name`, `phone`, `email`, `birthday`) VALUES ('{$new_user['name']}', '{$new_user['phone']}', '{$new_user['email']}', '{$new_user['birthday']}')";
+    //$sql_query = "INSERT INTO `customers` (`name`, `phone`, `email`, `birthday`) VALUES ('$name', '$phone', '$email', '$birthday')";
+    echo $sql_query . "\n";
+    $result = mysqli_query($mysqli, $sql_query);
+    };
+    
+    if($result){
+        echo 'Entry Successfull, Welcome!';
+    }
+    else{
+        echo 'Error Ocurred';
+    };
+
+
+    //works
+    $sql_delete = "DELETE FROM customers WHERE name = 'Bert Reynolds'";
+    $result2 = mysqli_query($mysqli, $sql_delete);
+};
+
+?>
---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]>      <html class="no-js"> <!--<![endif]-->
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>test sql</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="">
+    </head>
+    <body>
+
+
+
+        <form action="connect.php" method="POST">
+            <label for="name">Name:</label>
+            <input type="text" name="name" id="name" required>
+            <br><br>
+
+            <label for="phone">Phone Number:</label>
+            <input type="text" name="phone" id="phone" required>
+            <br><br>
+
+            <label for="email">Email:</label>
+            <input type="email" name="email" id="email" required>
+            <br><br>
+
+            <label for="birthday">Birthday:</label>
+            <input type="date" name="birthday" id="birthday" required>
+            <br><br>
+
+            <input type="submit" name="submit" id="submit">
+            
+        </form>
+        <!--[if lt IE 7]>
+            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+        
+        <script src="" async defer></script>
+    </body>
+</html>
