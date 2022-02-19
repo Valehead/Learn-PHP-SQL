c49fdce0df1daae26d124f6eea6475e14ad4aa74
Valehead <iquicksnipe@gmail.com>
2022-02-19 15:02:32 -0500

    testing prior query concept

--- connect.php
+++ connect.php
@@ -1,11 +1,11 @@
 <?php
 
-$new_user = [
-    "name" => "",
-    "phone" => "",
-    "email" => "",
-    "birthday" => ""
-];
+// $new_user = [
+//     "name" => "",
+//     "phone" => "",
+//     "email" => "",
+//     "birthday" => ""
+// ];
 
 
 if($_SERVER['REQUEST_METHOD'] == 'POST'){
@@ -15,13 +15,13 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
         foreach($new_user as $key => $value){
             $new_user[$key] = $_POST[$key];
         };
-        // $name = $_POST['name'];
-        // $phone = $_POST['phone'];
-        // $email = $_POST['email'];
-        // $birthday = $_POST['birthday'];
+        $name = $_POST['name'];
+        $phone = $_POST['phone'];
+        $email = $_POST['email'];
+        $birthday = $_POST['birthday'];
 
-    $sql_query = "INSERT INTO `customers` (`name`, `phone`, `email`, `birthday`) VALUES ('{$new_user['name']}', '{$new_user['phone']}', '{$new_user['email']}', '{$new_user['birthday']}')";
-    //$sql_query = "INSERT INTO `customers` (`name`, `phone`, `email`, `birthday`) VALUES ('$name', '$phone', '$email', '$birthday')";
+    //$sql_query = "INSERT INTO `customers` (`name`, `phone`, `email`, `birthday`) VALUES ('{$new_user['name']}', '{$new_user['phone']}', '{$new_user['email']}', '{$new_user['birthday']}')";
+    $sql_query = "INSERT INTO `customers` (`name`, `phone`, `email`, `birthday`) VALUES ('{$name}', '{$phone}', '{$email}', '{$birthday}')";
     echo $sql_query . "\n";
     $result = mysqli_query($mysqli, $sql_query);
     };
@@ -35,8 +35,8 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
 
 
     //works
-    $sql_delete = "DELETE FROM customers WHERE name = 'Bert Reynolds'";
-    $result2 = mysqli_query($mysqli, $sql_delete);
+    // $sql_delete = "DELETE FROM customers WHERE name = 'Bert Reynolds'";
+    // $result2 = mysqli_query($mysqli, $sql_delete);
 };
 
 ?>
