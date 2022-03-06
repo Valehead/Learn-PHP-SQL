27ac28299fa90af9c0d349fce450dc0f619142ed
Valehead <iquicksnipe@gmail.com>
2022-03-06 12:50:58 -0500

    sizing and scrolling

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ require __DIR__ . '/connect.php';
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-        <link rel="stylesheet" href="">
+        <link rel="stylesheet" href="index.css">
 
         <script>
             function clear_form(){
