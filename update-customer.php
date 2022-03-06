fa4115172752e39304df5bd442ac12644d241b52
Valehead <iquicksnipe@gmail.com>
2022-03-06 15:36:46 -0500

    separating mysqli database open to config.php

--- update-customer.php
+++ update-customer.php
@@ -7,7 +7,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
     //if the form sent valid data, fill the initialized user with data
     if(isset($_POST['id']) && isset($_POST['first-name']) && isset($_POST['last-name']) && isset($_POST['phone']) && isset($_POST['email']) && isset($_POST['birthday'])){
         
-        $sql_query = "UPDATE `customers` SET `first-name`={$_POST['first-name']}, `last-name`={$_POST['last-name']}, `phone`={$_POST['phone']}, `email`={$_POST['email']},`birthday`={$_POST['birthday']} WHERE id={$_POST['id']}";
+        $sql_query = "UPDATE `customers` SET `first-name`='{$_POST['first-name']}', `last-name`='{$_POST['last-name']}', `phone`='{$_POST['phone']}', `email`='{$_POST['email']}',`birthday`='{$_POST['birthday']}' WHERE id='{$_POST['id']}'";
         $result = mysqli_query($mysqli, $sql_query);
     };
     
