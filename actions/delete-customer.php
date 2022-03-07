ba2755553af45093c456881c4d7bcc569e379a0d
Valehead <iquicksnipe@gmail.com>
2022-03-07 00:17:06 -0500

    delete functionality

--- actions/delete-customer.php
+++ actions/delete-customer.php
@@ -6,16 +6,16 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
 
     if(isset($_POST['deleteItem']))
     {
-        $sql_query = "DELETE FROM `customers` WHERE id={$_POST['id']}";
+        $sql_query = "DELETE FROM `customers` WHERE id={$_POST['deleteItem']}";
         $result = mysqli_query($mysqli, $sql_query);
     };
 
-    // if($result){
-    //     header('Location: ../index.php');
-    // }
-    // else{
-    //     echo 'Error Ocurred' . mysqli_error($mysqli);
-    // };
+    if($result){
+        header('Location: ../index.php');
+    }
+    else{
+        echo 'Error Ocurred' . mysqli_error($mysqli);
+    };
 
     //close active connection
     mysqli_close($mysqli);
