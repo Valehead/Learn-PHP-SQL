88b28e9b760a237092f42296b780b88a9b0efd12
Valehead <iquicksnipe@gmail.com>
2022-03-06 23:59:39 -0500

    folder organization

--- actions/update-customer.php
+++ actions/update-customer.php
@@ -12,7 +12,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
     };
     
     if($result){
-        header('Location: index.php');
+        header('Location: ../index.php');
     }
     else{
         echo 'Error Ocurred' . mysqli_error($mysqli);
