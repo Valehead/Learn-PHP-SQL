0b281ba90eccac8c6efd3b1012cd9c2dff227c8e
Valehead <38196023+Valehead@users.noreply.github.com>
2022-03-07 10:08:43 -0500

    Update config.php
    duplicate line

--- config.php
+++ config.php
@@ -1,14 +1,12 @@
 <?php
 
-$mysqli = mysqli_connect('localhost', 'valehead', 'the4kingdb$', 'ripnship') or die("Connection Failed: " . mysqli_connect_error());
-
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'valehead');
 define('DB_PASSWORD', 'the4kingdb$');
 define('DB_NAME', 'ripnship');
  
 // Attempt to connect to MySQL database
-$mysqli = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+$mysqli = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME) or die("Connection Failed: " . mysqli_connect_error());
  
 // Check connection
 if($mysqli === false){
