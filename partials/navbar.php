293c61fed502b7c83ca865da3c85a0a6cc636466
Nick Vales <iquicksnipe@gmail.com>
2022-03-30 14:12:41 -0400

    start of navbar

--- partials/navbar.php
+++ partials/navbar.php
@@ -1,5 +1,11 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container-fluid">
-            <a href="/Learn-PHP-SQL" class="navbar-brand">Rip N Ship</a>
-        </div>
+
+    <div class="container-fluid">
+        <a href="/Learn-PHP-SQL" class="navbar-brand">Rip N Ship</a>
+    </div>
+
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    
 </nav>
