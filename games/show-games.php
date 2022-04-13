f800cab4d7e07228454a308e39ba5dc508a9a8df
Valehead <iquicksnipe@gmail.com>
2022-04-12 22:54:45 -0400

    change games page

--- games/show-games.php
+++ games/show-games.php
@@ -101,7 +101,7 @@
                 </div>
 
                 <!-- start of games table -->
-                <div class="col-3">
+                <div class="col-2">
                     <table class="table border">
                         <thead>
                             <tr>
