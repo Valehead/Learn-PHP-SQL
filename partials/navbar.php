bb7a6177de40799b50554e4dd336d2ad9693af2a
Nick Vales <iquicksnipe@gmail.com>
2022-04-13 12:44:11 -0400

    search box changes

--- partials/navbar.php
+++ partials/navbar.php
@@ -31,10 +31,11 @@
                     <a class="nav-link disabled">Accounts</a>
                 </li>
             </ul>
-            
+
+
             <form action="/Learn-PHP-SQL/customers/search-customers.php" method="get" class="d-flex">
-                <input class="form-control me-2" type="search" placeholder="Search Customers..." aria-label="Search">
-                <button class="btn btn-outline-success" type="submit">Search</button>
+                <input class="form-control me-2" name="searchBox" type="search" id="mySearch" placeholder="Search Customers..." aria-label="Search">
+                <button class="btn btn-outline-success" type="submit" id="submit">Search</button>
             </form>
         </div>
     </div>
