bee3db7ab58758a02bc79e5485538b3763551bb5
Nick Vales <iquicksnipe@gmail.com>
2022-04-13 13:11:11 -0400

    navbar sticky not fixed

--- partials/navbar.php
+++ partials/navbar.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
 
     <div class="container-fluid">
         <a href="/Learn-PHP-SQL" class="navbar-brand">Learn-PHP-SQL</a>
