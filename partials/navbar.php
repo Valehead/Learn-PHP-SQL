dd868c7c1e9bd9d9aa45cc2d09eafaa15459a5f1
Valehead <iquicksnipe@gmail.com>
2022-04-13 00:28:53 -0400

    navbar active links

--- partials/navbar.php
+++ partials/navbar.php
@@ -22,7 +22,8 @@
                 </li>
                 
                 <li class="nav-item">
-                    <a class="nav-link <?php if($_SERVER['SCRIPT_NAME']=="/Learn-PHP-SQL/games/show-games.php" || $_SERVER['SCRIPT_NAME']=="/Learn-PHP-SQL/games/edit-game.php") { echo 'active'; }; ?>" 
+                    <a class="nav-link <?php if($_SERVER['SCRIPT_NAME']=="/Learn-PHP-SQL/games/show-games.php" || 
+                    $_SERVER['SCRIPT_NAME']=="/Learn-PHP-SQL/games/edit-game.php") { echo 'active'; }; ?>" 
                     href="/Learn-PHP-SQL/games/show-games.php">Games</a>
                 </li>
                 
