b747068637be8f89dfea23c5267b30efe4d694be
Valehead <iquicksnipe@gmail.com>
2022-05-12 21:46:49 -0400

    finished the registration code in auth time for testing

--- src/libs/accounts/auth.php
+++ src/libs/accounts/auth.php
@@ -1,5 +1,32 @@
 <?php
+// require the sql config data
+require_once($_SERVER['DOCUMENT_ROOT'] .'/Learn-PHP-SQL/connect.php');
 
-function register_user(): bool
+
+function register_user(string $email, string $username, string $password, bool $is_admin = false): bool {
+
+    $user = array(
+        "email" => $email,
+        "username" => $username,
+        "password" => $password
+    );
+
+    //escape everything and recreate the object
+    foreach($user as $key => $value)
+    {
+        $user[$key] = $conn->real_escape_string($user[$key]);
+    };
+
+    $user['password'] = password_hash($user['password'], PASSWORD_BCRYPT);
+
+    //create the insert query, execute the query and save the query result
+    $result = $conn->query("INSERT INTO `users` (`email`, `username`, `password`, `is_admin`) 
+    VALUES ('{$user['email']}', '{$user['username']}', '{$user['password']}', '{$is_admin}');");
+    
+
+    //return the success or fail
+    return $result;
+
+};
 
 ?>
