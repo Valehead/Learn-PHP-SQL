deb38b1c081095877940738bc86b7c298a940a85
Nick Vales <iquicksnipe@gmail.com>
2022-05-23 13:58:12 -0400

    adding display test code

--- accounts/register.php
+++ accounts/register.php
@@ -2,9 +2,7 @@
 
 //tie in our session and all our helper files
 require_once($_SERVER['DOCUMENT_ROOT'] .'/Learn-PHP-SQL/src/libs/accounts/register.php');
-print_r($inputs);
-print_r($errors);
-echo $_SERVER['REQUEST_METHOD'];
+
 
 ?>
 
