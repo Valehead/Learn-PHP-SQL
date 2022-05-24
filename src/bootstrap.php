6c694a080237a042f51a57b3e54c49cb2088cc47
Nick Vales <iquicksnipe@gmail.com>
2022-05-24 10:51:07 -0400

    adding session start code to navbar

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -1,6 +1,5 @@
 <?php
 
-session_start();
 require_once($_SERVER['DOCUMENT_ROOT'] .'/Learn-PHP-SQL/connect.php');
 require_once __DIR__ . '/libs/helpers.php';
 require_once __DIR__ . '/libs/validation.php';
