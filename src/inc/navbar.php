01b4afc817c942f0444a833a42c7ea30d5d8745b
Valehead <iquicksnipe@gmail.com>
2022-05-24 23:53:15 -0400

    navbar styling

--- src/inc/navbar.php
+++ src/inc/navbar.php
@@ -32,44 +32,35 @@ require_once($_SERVER['DOCUMENT_ROOT'] .'/Learn-PHP-SQL/src/bootstrap.php');
                     href="/Learn-PHP-SQL/games/show-games.php">Games</a>
                 </li>
                 
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle <?php if($_SERVER['SCRIPT_NAME']=="/Learn-PHP-SQL/accounts/login.php" || 
-                    $_SERVER['SCRIPT_NAME']=="/Learn-PHP-SQL/accounts/register.php") { echo 'active'; }; ?>"
-                     id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">Accounts</a>
-                    
-                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <li><a href="/Learn-PHP-SQL/accounts/login.php" class="dropdown-item">Login</a></li>
-                        <li><a href="/Learn-PHP-SQL/accounts/register.php" class="dropdown-item">Sign Up</a></li>
+                <?php if(is_user_logged_in()){?>
+                    <ul class="navbar-nav">
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" id="navbarDropdown2" 
+                            role="button" data-bs-toggle="dropdown" aria-expanded="false"
+                            href="">My Account</a>
+
+                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown2">
+                                <li><span class="dropdown-item-text">Welcome: <a href="/Learn-PHP-SQL/accounts/my-account.php"><?= $_SESSION['username']; ?></a></span></li>
+                                <li><a href="/Learn-PHP-SQL/accounts/logout.php" class="dropdown-item">Log Out</a></li>
+                            </ul>
+                        </li>
                     </ul>
-                </li>
+                <?php };?>
             </ul>
             
-           <?php if(is_user_logged_in()){?>
-            <ul class="navbar-nav">
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" id="navbarDropdown2" 
-                    role="button" data-bs-toggle="dropdown" aria-expanded="false"
-                    href="">My Account</a>
 
-                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown2">
-                        <li><span class="dropdown-item-text">Welcome: <a href="/Learn-PHP-SQL/accounts/my-account.php"><?= $_SESSION['username']; ?></a></span></li>
-                        <li><a href="/Learn-PHP-SQL/accounts/logout.php" class="dropdown-item">Log Out</a></li>
-                    </ul>
-                </li>
-            </ul>
-             <?php };?>
                 
 
 
             <form action="/Learn-PHP-SQL/customers/search-customers.php" method="get" class="d-flex">
-                <input class="form-control  me-4" name="searchBox" type="search" id="myNavSearch" placeholder="Search Customers..." aria-label="Search">
+                <input class="form-control me-4" name="searchBox" type="search" id="myNavSearch" placeholder="Search Customers..." aria-label="Search">
             </form>
 
             <?php if(!is_user_logged_in()){ ?>
-            <div class="mt-3 mt-lg-0">
-                <a href="/Learn-PHP-SQL/accounts/login.php"><button class="btn btn-outline-light me-3">Login</button></a>
-                <a href="/Learn-PHP-SQL/accounts/register.php"><button class="btn btn-warning">Sign Up</button></a>
-            </div>
+                <div class="mt-3 mt-lg-0">
+                    <a href="/Learn-PHP-SQL/accounts/login.php"><button class="btn btn-outline-light me-3">Login</button></a>
+                    <a href="/Learn-PHP-SQL/accounts/register.php"><button class="btn btn-warning">Sign Up</button></a>
+                </div>
             <?php }; ?>
             
         </div>
