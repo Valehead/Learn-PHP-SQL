915baafa09a6281e7839ecaea84c787a96f93d11
Valehead <iquicksnipe@gmail.com>
2022-05-25 01:01:59 -0400

    login requirements

--- src/libs/games/delete-game.php
+++ src/libs/games/delete-game.php
@@ -1,6 +1,10 @@
 <?php
-require_once($_SERVER['DOCUMENT_ROOT'] .'/Learn-PHP-SQL/connect.php');
+require_once($_SERVER['DOCUMENT_ROOT'] .'/Learn-PHP-SQL/src/bootstrap.php');
 
+//only allow access to games page if someone is logged in
+if(!is_user_logged_in()){
+    redirect_to('/Learn-PHP-SQL/accounts/login.php?message=require_login');
+};
 
 if($_SERVER['REQUEST_METHOD'] == 'POST'){
 
--- src/libs/games/update-game.php
+++ src/libs/games/update-game.php
@@ -1,5 +1,10 @@
 <?php
-require_once($_SERVER['DOCUMENT_ROOT'] .'/Learn-PHP-SQL/connect.php');
+require_once($_SERVER['DOCUMENT_ROOT'] .'/Learn-PHP-SQL/src/bootstrap.php');
+
+//only allow access to games page if someone is logged in
+if(!is_user_logged_in()){
+    redirect_to('/Learn-PHP-SQL/accounts/login.php?message=require_login');
+};
 
 //check if there is a post request
 if($_SERVER['REQUEST_METHOD'] == 'POST'){
