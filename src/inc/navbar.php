fbf93aa1a28a660fd4dbc10ce8e3e1bad6002746
Valehead <iquicksnipe@gmail.com>
2022-05-29 16:54:34 -0400

    navbar style

--- src/inc/navbar.php
+++ src/inc/navbar.php
@@ -4,7 +4,7 @@ require_once($_SERVER['DOCUMENT_ROOT'] .'/Learn-PHP-SQL/src/bootstrap.php');
 
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
 
-    <div class="container-fluid py-1 mx-2">
+    <div class="container-fluid py-1 ms-2 me-4">
         <a href="/Learn-PHP-SQL" class="navbar-brand">Learn-PHP-SQL</a>
         
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -46,7 +46,7 @@ require_once($_SERVER['DOCUMENT_ROOT'] .'/Learn-PHP-SQL/src/bootstrap.php');
                     <a href="/Learn-PHP-SQL/accounts/register.php"><button class="btn btn-warning">Sign Up</button></a>
                 </div>
             <?php }; ?>
-            
+
             <?php if(is_user_logged_in()){?>
                     <ul class="navbar-nav">
                         <li class="nav-item dropdown">
