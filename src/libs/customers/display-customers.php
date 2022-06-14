3c740cf5da85bda3041ff8e55db4336fdbd59993
Valehead <iquicksnipe@gmail.com>
2022-06-14 00:11:05 -0400

    hiding new customer form unless logged in, reformatting home page

--- src/libs/customers/display-customers.php
+++ src/libs/customers/display-customers.php
@@ -24,22 +24,24 @@ function display_customers(){
             while($row = $result->fetch_assoc()) {
                 $row['birthday'] = date('F jS',strtotime($row['birthday']));
                 
-                echo "<div class='col'><div class='card mb-3' id='customer'>
-                        <div class='card-body'>
-                        <h4 class='card-title'>Customer Id: {$row['id']}</h4>
-                        <ul class='list-group list-group-flush'>
-                            <li class='list-group-item list-group-item-secondary' id='custName'>{$row['firstName']} {$row['lastName']}</li>
-                            <li class='list-group-item' id='phoneNum'>Phone: {$row['phone']}</li>
-                            <li class='list-group-item' id='email'>Email: {$row['email']}</li>
-                            <li class='list-group-item' id='birthday'>Birthday: {$row['birthday']}</li>
-                            <li class='list-group-item' id='custId' style='display:none;'>{$row['id']}</li>
-                        </ul>
-                        <div class='mt-2 mx-5 d-flex justify-content-between'>
-                            <a href='customers/view-customer.php?id={$row['id']}'><button type='button' class='btn btn-secondary' name='viewCustomer' id='viewCustomer'>View</button></a>
-                            <a href='customers/edit-customer.php?id={$row['id']}'><button type='button' class='btn btn-warning' name='editCustomer' id='editCustomer'>Edit</button></a>
+                echo "<div class='col-4'>
+                        <div class='card mb-3' id='customer'>
+                            <div class='card-body'>
+                            <h4 class='card-title'>Customer Id: {$row['id']}</h4>
+                            <ul class='list-group list-group-flush'>
+                                <li class='list-group-item list-group-item-secondary' id='custName'>{$row['firstName']} {$row['lastName']}</li>
+                                <li class='list-group-item' id='phoneNum'>Phone: {$row['phone']}</li>
+                                <li class='list-group-item' id='email'>Email: {$row['email']}</li>
+                                <li class='list-group-item' id='birthday'>Birthday: {$row['birthday']}</li>
+                                <li class='list-group-item' id='custId' style='display:none;'>{$row['id']}</li>
+                            </ul>
+                            <div class='mt-2 mx-5 d-flex justify-content-between'>
+                                <a href='customers/view-customer.php?id={$row['id']}'><button type='button' class='btn btn-secondary' name='viewCustomer' id='viewCustomer'>View</button></a>
+                                <a href='customers/edit-customer.php?id={$row['id']}'><button type='button' class='btn btn-warning' name='editCustomer' id='editCustomer'>Edit</button></a>
+                            </div>
+                            </div>
                         </div>
-                        </div>
-                        </div></div>";
+                    </div>";
             };
         } else {
             echo "<div class='card'><div class='card-body'><h4 class='card-title'>0 Customers</h4></div></div>";
