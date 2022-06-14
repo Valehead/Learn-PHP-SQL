3b69d1503c05c813cc0cd1b5100802059862d40d
Valehead <iquicksnipe@gmail.com>
2022-06-14 01:37:13 -0400

    hiding new customer form unless logged in, reformatting home page

--- accounts/logout.php
+++ accounts/logout.php
@@ -3,7 +3,7 @@ require_once($_SERVER['DOCUMENT_ROOT'] .'/Learn-PHP-SQL/src/bootstrap.php');
 
 //only allow access to logout page if someone is logged in
 if(!is_user_logged_in()){
-    redirect_to('/Learn-PHP-SQL/index.php');
+    redirect_to('/Learn-PHP-SQL/accounts/login.php');
 };
 
 logout();
