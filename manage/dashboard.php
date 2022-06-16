aeba3f48f206863f193829c6da1a90cf7df788da
Nick Vales <iquicksnipe@gmail.com>
2022-06-15 20:52:11 -0400

    chart start

--- manage/dashboard.php
+++ manage/dashboard.php
@@ -13,18 +13,7 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
         
         <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.8.0/chart.min.js" integrity="sha512-sW/w8s4RWTdFFSduOTGtk4isV1+190E/GghVffMA9XczdJ2MDzSzLEubKAs5h0wzgSJOQTRYyaz73L3d6RtJSg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-        <script>
-            var chartCanvas1 = document.getElementById('chart1');
-            var configCanvas1 = {
-                type: "bar",
-                data: {
-                    labels: ["Bill", "Jeff", "Michael", "Tim", "Zuck"], 
-                    datasets: [{label: "Number of Cookies", data: [5, 2, 12, 19, 3]}]
-                }
-            };
 
-            var myChart1 = new Chart(chartCanvas1, configCanvas1);
-        </script>
     </head>
     <body>
         <?php include($_SERVER['DOCUMENT_ROOT'] ."/Learn-PHP-SQL/src/inc/navbar.php") ?>
@@ -43,7 +32,18 @@
 
         </main>
 
-        
+        <script>
+            var chartCanvas1 = document.getElementById('chart1');
+            var configCanvas1 = {
+                type: "bar",
+                data: {
+                    labels: ["Bill", "Jeff", "Michael", "Tim", "Zuck"], 
+                    datasets: [{label: "Number of Cookies", data: [5, 2, 12, 19, 3]}]
+                }
+            };
+
+            var myChart1 = new Chart(chartCanvas1, configCanvas1);
+        </script>
         <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-kjU+l4N0Yf4ZOJErLsIcvOU2qSb74wXpOhqTvwVx3OElZRweTnQ6d31fXEoRD1Jy" crossorigin="anonymous"></script>
     </body>
