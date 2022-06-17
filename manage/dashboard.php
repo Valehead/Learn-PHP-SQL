d3530b8bb21222447a0029d64d1607c5ed0b5425
Nick Vales <iquicksnipe@gmail.com>
2022-06-16 20:30:24 -0400

    working on manager dashboard

--- manage/dashboard.php
+++ manage/dashboard.php
@@ -1,3 +1,12 @@
+<?php 
+require_once($_SERVER['DOCUMENT_ROOT'] .'/Learn-PHP-SQL/src/bootstrap.php');
+require_once($_SERVER['DOCUMENT_ROOT'] .'/Learn-PHP-SQL/src/libs/reports/admin-graphs.php');
+
+
+
+
+?>
+
 <!DOCTYPE html>
 <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
@@ -24,6 +33,7 @@
                     <div class="card mt-5" id="chart1card">
                         <div class="card-body">
                             <canvas id="chart1" style="width:100%;max-width:700px"></canvas>
+                            <?php print_r(get_all_games()); ?>
                         </div>
                     </div>
                 </div>
@@ -32,7 +42,38 @@
 
         </main>
 
-        <script src="../public/js/dashboardGraphs.js"></script>
+        <script src="../public/js/dashboardGraphs.js">
+            var chartCanvas1 = document.getElementById('chart1');
+            var configCanvas1 = {
+                type: "bar",
+                data: {
+                    labels: ["Bill", "Jeff", "Michael", "Tim", "Zuck"],
+                    datasets: [{
+                        label: "Number of Cookies",
+                        data: [5, 2, 12, 19, 3],
+                        backgroundColor: [
+                            'rgba(255, 99, 132, 0.2)',
+                            'rgba(54, 162, 235, 0.2)',
+                            'rgba(255, 206, 86, 0.2)',
+                            'rgba(75, 192, 192, 0.2)',
+                            'rgba(153, 102, 255, 0.2)',
+                            'rgba(255, 159, 64, 0.2)'
+                        ],
+                        borderColor: [
+                            'rgba(255, 99, 132, 1)',
+                            'rgba(54, 162, 235, 1)',
+                            'rgba(255, 206, 86, 1)',
+                            'rgba(75, 192, 192, 1)',
+                            'rgba(153, 102, 255, 1)',
+                            'rgba(255, 159, 64, 1)'
+                        ],
+                        borderWidth: 1,
+                    }]
+                }
+            };
+
+            var myChart1 = new Chart(chartCanvas1, configCanvas1);
+        </script>
         <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-kjU+l4N0Yf4ZOJErLsIcvOU2qSb74wXpOhqTvwVx3OElZRweTnQ6d31fXEoRD1Jy" crossorigin="anonymous"></script>
     </body>
