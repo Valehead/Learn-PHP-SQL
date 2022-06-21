e37bfd53735cf95a4ea3477e0b6c0b2f5f476736
Nick Vales <iquicksnipe@gmail.com>
2022-06-21 12:27:57 -0400

    messing with the charts

--- src/libs/reports/admin-graphs.php
+++ src/libs/reports/admin-graphs.php
@@ -12,14 +12,14 @@ function get_all_games(){
     //build and execute the query
     //$result = $conn->query("SELECT `gameTitle` FROM `games` ORDER BY `id`;");
     $result = $conn->query("SELECT g.gameTitle, COUNT(*) as count FROM games g 
-    right JOIN gamesPlayed p 
-    on g.id = p.gameId
-    GROUP BY g.gameTitle
-    ORDER BY count desc;");
+                            right JOIN gamesPlayed p 
+                            on g.id = p.gameId
+                            GROUP BY g.gameTitle
+                            ORDER BY count desc;");
 
 
     //close active connection
-    //$conn->close();
+    $conn->close();
 
     //if we got a valid result....
     if($result){
@@ -27,22 +27,10 @@ function get_all_games(){
         //and there is a result including info....
         if($result->num_rows >0){
             return $result->fetch_all(MYSQLI_NUM);
-            // //iterate over the data and create rows for each game
-            // while($row = $result->fetch_assoc()){
-                
-            //     echo "<tr id='game'>
-            //             <th scope='row'><a href='/Learn-PHP-SQL/games/edit-game.php?id={$row['id']}'>{$row['id']}</a></th>
-            //             <td>{$row['gameTitle']}</td>
-            //           </tr>";
-            // };
         //if we get a result from sql but no data
-        } else {
-            echo "<tr id='game'>
-                    <th scope='row'>0</th>
-                    <td>No Games Found</td>
-                  </tr>";
         };
     };
+    return;
 };
 
 
@@ -56,10 +44,10 @@ function how_many_games(){
     //build and execute the query
     //$result = $conn->query("SELECT `gameTitle` FROM `games` ORDER BY `id`;");
     $result = $conn->query("SELECT CONCAT_WS(' ', c.firstName, c.lastName) AS `whole_name`, COUNT(p.customerId) as count FROM customers c 
-    right JOIN gamesPlayed p 
-    on c.id = p.customerId
-    GROUP BY whole_name 
-    ORDER BY count desc");
+                            right JOIN gamesPlayed p 
+                            on c.id = p.customerId
+                            GROUP BY whole_name 
+                            ORDER BY count desc");
 
 
     //close active connection
@@ -71,22 +59,9 @@ function how_many_games(){
         //and there is a result including info....
         if($result->num_rows >0){
             return $result->fetch_all(MYSQLI_NUM);
-            // //iterate over the data and create rows for each game
-            // while($row = $result->fetch_assoc()){
-                
-            //     echo "<tr id='game'>
-            //             <th scope='row'><a href='/Learn-PHP-SQL/games/edit-game.php?id={$row['id']}'>{$row['id']}</a></th>
-            //             <td>{$row['gameTitle']}</td>
-            //           </tr>";
-            // };
-        //if we get a result from sql but no data
-        } else {
-            echo "<tr id='game'>
-                    <th scope='row'>0</th>
-                    <td>No Games Found</td>
-                  </tr>";
         };
     };
+    return;
 };
 
 ?>
