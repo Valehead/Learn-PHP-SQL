7a0467d39656e2d843c5175babee74a5c2757ac5
Nick Vales <iquicksnipe@gmail.com>
2022-06-28 13:39:16 -0400

    nav css

--- manage/dashboard.php
+++ manage/dashboard.php
@@ -3,7 +3,7 @@ require_once($_SERVER['DOCUMENT_ROOT'] .'/Learn-PHP-SQL/src/bootstrap.php');
 require_once($_SERVER['DOCUMENT_ROOT'] .'/Learn-PHP-SQL/src/libs/reports/admin-graphs.php');
 
 //only allow access if admin is logged in
-if(is_user_an_admin()){
+if(!is_user_an_admin()){
     redirect_to('/Learn-PHP-SQL/accounts/login.php?message=require_admin');
 };
 
